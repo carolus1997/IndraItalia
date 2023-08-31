3af10ae14e3f25f5e389e1a11069612e549d1414
Carlos <carolus1997@hotmail.com>
2023-08-31 12:01:09 +0200

    31/08/2023_1

---
+++ ExtraccionAlturasEdificios.py
@@ -0,0 +1,33 @@
+import arcpy
+import json
+import os
+
+# Define la función para extraer y convertir los datos
+def extract_height(properties):
+    data = json.loads(properties)
+    height = data['height']
+    height_str = str(height)
+    return height_str.replace('.', ',')
+
+# Define la ruta de la carpeta que deseas recorrer
+workspace = "ruta_a_tu_carpeta"
+
+# Recorre la carpeta y subcarpetas
+for dirpath, dirnames, filenames in arcpy.da.Walk(workspace, datatype="FeatureClass"):
+    for filename in filenames:
+        # Define la ruta completa al shapefile
+        fc = os.path.join(dirpath, filename)
+
+        # Agrega un nuevo campo de tipo float llamado "altura" si no existe
+        if 'altura' not in [f.name for f in arcpy.ListFields(fc)]:
+            arcpy.AddField_management(fc, "altura", "FLOAT")
+
+        # Usa un cursor de actualización para recorrer cada fila en el shapefile
+        with arcpy.da.UpdateCursor(fc, ["properties", "altura"]) as cursor:
+            for row in cursor:
+                # Extrae y convierte los datos del campo "properties"
+                altura = extract_height(row[0])
+
+                # Actualiza el valor del campo "altura"
+                row[1] = altura
+                cursor.updateRow(row)
