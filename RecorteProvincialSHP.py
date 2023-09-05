63759f869e2e5d99f02b32d9b4f0217a3dbc9cc8
Carlos <carolus1997@hotmail.com>
2023-09-05 13:06:56 +0200

    Exito en recorte shp

--- RecorteProvincialSHP.py
+++ RecorteProvincialSHP.py
@@ -1,4 +1,8 @@
 import arcpy
+import os
+import re
+
+arcpy.env.overwriteOutput = True
 
 # Define las rutas de las capas de entrada, la capa de provincias y el geodatabase de salida
 capa_entrada = input("Introduce la ruta de la capa a recortar: ")
@@ -12,14 +16,20 @@ arcpy.MakeFeatureLayer_management(capa_provincias, 'provincias_lyr')
 # Obtiene una lista de las provincias en la capa de provincias
 provincias = [row[0] for row in arcpy.da.SearchCursor('provincias_lyr', 'NAME_2')]
 
-print(f"LISTA DE PRONVIAS: {provincias}")
+# Sustituye las comillas simples en los nombres de las provincias por un carácter de escape de comilla simple
+provincias_escaped = [provincia.replace("'", "_") for provincia in provincias]
+
+print(f"LISTA DE PRONVINCIAS: {provincias_escaped}")
+
 # Recorre cada provincia
-for provincia in provincias:
+for provincia in provincias_escaped:
     # Selecciona la provincia actual
     arcpy.SelectLayerByAttribute_management('provincias_lyr', 'NEW_SELECTION', f"NAME_2 = '{provincia}'")
 
     # Define el nombre de la capa de salida
-    capa_salida = f"{gdb_salida}/capa_entrada_{provincia}"
+    nombre_capa_salida = f"rios_{provincia}"
+    nombre_capa_salida = nombre_capa_salida.replace(" ", "_").replace("-", "_")
+    capa_salida = os.path.join(gdb_salida, nombre_capa_salida)
 
     # Recorta la capa de entrada según los límites de la provincia actual y guarda el resultado en la capa de salida
     arcpy.Clip_analysis(capa_entrada, 'provincias_lyr', capa_salida)
@@ -28,3 +38,4 @@ for provincia in provincias:
 arcpy.Delete_management('provincias_lyr')
 
 print("PROCESO TERMINADO")
+
