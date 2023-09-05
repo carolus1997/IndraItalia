c902c366308b4abecaf9247ac087983419d1f0b3
Carlos <carolus1997@hotmail.com>
2023-09-05 11:10:19 +0200

    05/09/2023

--- RecorteProvincialSHP.py
+++ RecorteProvincialSHP.py
@@ -3,7 +3,8 @@ import arcpy
 # Define las rutas de las capas de entrada, la capa de provincias y el geodatabase de salida
 capa_entrada = input("Introduce la ruta de la capa a recortar: ")
 capa_provincias = input("Introduce la ruta de la capa de provincias: ")
-gdb_salida = 'ruta/gdb_salida.gdb'
+gdb_output = input("Introduce la ruta de la gdb de destino: ")
+gdb_salida = gdb_output
 
 # Crea una capa temporal de la capa de provincias
 arcpy.MakeFeatureLayer_management(capa_provincias, 'provincias_lyr')
@@ -11,6 +12,7 @@ arcpy.MakeFeatureLayer_management(capa_provincias, 'provincias_lyr')
 # Obtiene una lista de las provincias en la capa de provincias
 provincias = [row[0] for row in arcpy.da.SearchCursor('provincias_lyr', 'NAME_2')]
 
+print(f"LISTA DE PRONVIAS: {provincias}")
 # Recorre cada provincia
 for provincia in provincias:
     # Selecciona la provincia actual
@@ -24,3 +26,5 @@ for provincia in provincias:
 
 # Elimina la capa temporal de provincias
 arcpy.Delete_management('provincias_lyr')
+
+print("PROCESO TERMINADO")
