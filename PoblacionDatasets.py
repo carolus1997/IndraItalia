f9fa1142b820c1590f7d9317ece6597a969f75e8
Carlos <carolus1997@hotmail.com>
2023-09-18 16:22:31 +0200

    x

--- PoblacionDatasets.py
+++ PoblacionDatasets.py
@@ -31,11 +31,11 @@ def mapear_nombre_provincia_a_dataset(nombre_provincia):
 
 
 # Obtener la ruta inicial del usuario
-ruta_inicial = r"C:\Users\ctmiraperceval\Desktop\CartoItalia\Data"
+ruta_inicial = r"C:\Users\ctmiraperceval\OneDrive - Indra\Escritorio\CartoItalia\Data"
 
 # Construir las rutas completas a las carpetas de ríos y carreteras
-ruta_recursos_rios = os.path.join(ruta_inicial, "Recursos", "Rios")
-ruta_recursos_carreteras = os.path.join(ruta_inicial, "Recursos", "Carreteras")
+ruta_recursos_rios = os.path.join(ruta_inicial, "Recursos", "CuerposAgua")
+#ruta_recursos_carreteras = os.path.join(ruta_inicial, "Recursos", "Vegetacion")
 ruta_resultados = os.path.join(ruta_inicial, "Resultados", "Gdbs")
 
 # Crear las carpetas de resultados si no existen
@@ -57,20 +57,20 @@ if archivos_rios:
 else:
     print("No se encontraron archivos en la carpeta de ríos.")
 
-# Obtener la lista de archivos en la carpeta de carreteras
-archivos_carreteras = os.listdir(ruta_recursos_carreteras)
-capas_carreteras = []
-if archivos_carreteras:
-    for archivo in archivos_carreteras:
-        if archivo.endswith(".gdb"):
-            gdb_carretera = os.path.join(ruta_recursos_carreteras, archivo)
-            arcpy.env.workspace = gdb_carretera
-            try:
-                capas_carreteras = arcpy.ListFeatureClasses()
-            except arcpy.ExecuteError:
-                print(arcpy.GetMessages())
-else:
-    print("No se encontraron archivos en la carpeta de carreteras.")
+# # Obtener la lista de archivos en la carpeta de carreteras
+# archivos_carreteras = os.listdir(ruta_recursos_carreteras)
+# capas_carreteras = []
+# if archivos_carreteras:
+#     for archivo in archivos_carreteras:
+#         if archivo.endswith(".gdb"):
+#             gdb_carretera = os.path.join(ruta_recursos_carreteras, archivo)
+#             arcpy.env.workspace = gdb_carretera
+#             try:
+#                 capas_carreteras = arcpy.ListFeatureClasses()
+#             except arcpy.ExecuteError:
+#                 print(arcpy.GetMessages())
+# else:
+#     print("No se encontraron archivos en la carpeta de carreteras.")
 
 archivos_resultados = os.listdir(ruta_resultados)
 for archivo in archivos_resultados:
@@ -87,21 +87,21 @@ for archivo in archivos_resultados:
                 if mapear_nombre_provincia_a_dataset(nombre_provincia_rios) == dataset:
                     # Copiar la capa a este dataset de resultados
                     capa_origen_rios = os.path.join(gdb_rio, rio)
-                    capa_destino_rios = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_rios)}_rios")
+                    capa_destino_rios = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_rios)}_CuerposAgua")
                     if not arcpy.Exists(capa_destino_rios):
                         try:
                             arcpy.CopyFeatures_management(capa_origen_rios, capa_destino_rios)
                         except arcpy.ExecuteError:
                             print(arcpy.GetMessages())
-            for carretera in capas_carreteras:
-                partes_nombre = carretera.split("_")
-                nombre_provincia_carreteras = "_".join(partes_nombre[1:])
-                if mapear_nombre_provincia_a_dataset(nombre_provincia_carreteras) == dataset:
-                    # Copiar la capa a este dataset de resultados
-                    capa_origen_carreteras = os.path.join(gdb_carretera, carretera)
-                    capa_destino_carreteras = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_carreteras)}_carreteras")
-                    if not arcpy.Exists(capa_destino_carreteras):
-                        try:
-                            arcpy.CopyFeatures_management(capa_origen_carreteras, capa_destino_carreteras)
-                        except arcpy.ExecuteError:
-                            print(arcpy.GetMessages())
+            # for carretera in capas_carreteras:
+            #     partes_nombre = carretera.split("_")
+            #     nombre_provincia_carreteras = "_".join(partes_nombre[1:])
+            #     if mapear_nombre_provincia_a_dataset(nombre_provincia_carreteras) == dataset:
+            #         # Copiar la capa a este dataset de resultados
+            #         capa_origen_carreteras = os.path.join(gdb_carretera, carretera)
+            #         capa_destino_carreteras = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_carreteras)}_Vegetacion")
+            #         if not arcpy.Exists(capa_destino_carreteras):
+            #             try:
+            #                 arcpy.CopyFeatures_management(capa_origen_carreteras, capa_destino_carreteras)
+            #             except arcpy.ExecuteError:
+            #                 print(arcpy.GetMessages())
