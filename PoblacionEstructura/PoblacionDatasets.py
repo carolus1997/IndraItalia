1ad71fa9649e068fc38a39dfadaea0c572c8d287
Carlos <carolus1997@hotmail.com>
2023-11-13 14:32:11 +0100

    x

--- PoblacionEstructura/PoblacionDatasets.py
+++ PoblacionEstructura/PoblacionDatasets.py
@@ -21,6 +21,13 @@ def mapear_nombre_provincia_a_dataset(nombre_provincia):
 
     return nombre_provincia
 
+#  copiar raster o feature class
+def copiar_recurso(origen, destino, tipo):
+    if tipo == "FeatureClass":
+        arcpy.CopyFeatures_management(origen, destino)
+    elif tipo == "Raster":
+        arcpy.CopyRaster_management(origen, destino)
+
 # Obtener la ruta inicial del usuario
 ruta_inicial = r"D:\Minsait_Curro"
 
@@ -29,29 +36,34 @@ ruta_recursos = input("Introduce la ruta de la capa de entrada: ")
 ruta_resultados = input("Introduce la ruta de la carpeta de destino: ")
 
 tipocapa = input("Introduce el tipo de capa que vas a usar por ej. rios, carreteras, etc: ")
+
 # Crear las carpetas de resultados si no existen
 if not os.path.exists(ruta_resultados):
     os.makedirs(ruta_resultados)
 
-# Obtener la lista de archivos en la carpeta de ríos
+# Procesar archivos en la carpeta de recursos
 archivos_recurso = os.listdir(ruta_recursos)
-capas_recurso = []
+recursos = []
 
 try:
     for archivo in archivos_recurso:
         if archivo.endswith(".gdb"):
             gdb_origen = os.path.join(ruta_recursos, archivo)
             arcpy.env.workspace = gdb_origen
-            capas = arcpy.ListFeatureClasses()
-            if capas:
-                capas_recurso.extend(capas)
+            feature_classes = arcpy.ListFeatureClasses()
+            rasters = arcpy.ListRasters()
+
+            # Agregar feature classes y rasters a la lista de recursos
+            recursos.extend([(fc, "FeatureClass") for fc in feature_classes])
+            recursos.extend([(raster, "Raster") for raster in rasters])
 except Exception as e:
-    print(f"Error al listar las capas: {e}")
+    print(f"Error al listar los recursos: {e}")
 
-if not capas_recurso:
-    print("No se encontraron capas en la carpeta de recursos.")
-    exit()  # Terminar el script si no hay capas
+if not recursos:
+    print("No se encontraron recursos en la carpeta de recursos.")
+    exit()
 
+# Procesar archivos en la carpeta de resultados
 try:
     archivos_resultados = os.listdir(ruta_resultados)
     for archivo in archivos_resultados:
@@ -60,13 +72,13 @@ try:
             arcpy.env.workspace = gdb_resultado
             datasets = arcpy.ListDatasets("*", "Feature")
             for dataset in datasets:
-                for capa in capas_recurso:
-                    nombre_provincia_capa = obtener_nombre_dataset(capa)
-                    if mapear_nombre_provincia_a_dataset(nombre_provincia_capa) == dataset:
-                        capa_origen = os.path.join(gdb_origen, capa)
-                        capa_destino = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_capa)}_{tipocapa}")
-                        if not arcpy.Exists(capa_destino):
-                            arcpy.CopyFeatures_management(capa_origen, capa_destino)
-                            print(f"Se ha copiado la capa: {capa_destino}")
+                for recurso, tipo_recurso in recursos:
+                    nombre_provincia_recurso = obtener_nombre_dataset(recurso)
+                    if mapear_nombre_provincia_a_dataset(nombre_provincia_recurso) == dataset:
+                        recurso_origen = os.path.join(gdb_origen, recurso)
+                        recurso_destino = os.path.join(gdb_resultado, dataset, f"{mapear_nombre_provincia_a_dataset(nombre_provincia_recurso)}_{tipocapa}")
+                        if not arcpy.Exists(recurso_destino):
+                            copiar_recurso(recurso_origen, recurso_destino, tipo_recurso)
+                            print(f"Se ha copiado el recurso: {recurso_destino}")
 except Exception as e:
     print(f"Error durante el proceso: {e}")
--- PoblacionEstructura/RecorteProvinciasRaster.py
+++ PoblacionEstructura/RecorteProvinciasRaster.py
@@ -9,11 +9,11 @@ capa_entrada = input("Introduce la ruta de la capa a recortar: ")
 capa_provincias = input("Introduce la ruta de la capa de provincias: ")
 gdb_output = input("Introduce la ruta de la gdb de destino: ")
 gdb_name = input("Introduce el nombre de la gdb de destino: ")
-gdb_salida = os.path.join(gdb_output,gdb_name)
+gdb_salida = os.path.join(gdb_output, gdb_name)
+
 if arcpy.Exists(gdb_salida):
     print("Tu gdb ya existe")
-    vaciado = input(
-        "¿Quieres hacerle un vaciado de seguridad,(Responde usando mayusculas)? ")
+    vaciado = input("¿Quieres hacerle un vaciado de seguridad (Responde usando mayusculas)? ")
     if vaciado == 'SI':
         arcpy.env.workspace = gdb_salida
         featureclasses_existentes = arcpy.ListFeatureClasses()
@@ -24,18 +24,16 @@ if arcpy.Exists(gdb_salida):
         print("Perfecto, dejamos tu gdb intacta")
 else:
     print("Tu gdb no existe")
-    input_pregunta_gdb = input(f"¿Quieres usar la ruta {gdb_output} ?")
+    input_pregunta_gdb = input(f"¿Quieres usar la ruta {gdb_output}? ")
     if input_pregunta_gdb == 'SI':
-        input_name_gdb = input(
-            "Introduce el nombre de la GDB que vas a crear: ")
+        input_name_gdb = input("Introduce el nombre de la GDB que vas a crear: ")
         arcpy.management.CreateFileGDB(gdb_output, input_name_gdb)
         print(f"Se ha creado una .gdb con el nombre: {input_name_gdb}")
     elif input_pregunta_gdb == 'NO':
         input_new_gdb = input("Introduce la ruta de la GDB que vas a crear: ")
         new_gdb = input_new_gdb
         print(new_gdb)
-        input_name_gdb = input(
-            "Introduce el nombre de la GDB que vas a crear: ")
+        input_name_gdb = input("Introduce el nombre de la GDB que vas a crear: ")
         new_name = f"{input_name_gdb}.gdb"
         print(new_name)
         arcpy.management.CreateFileGDB(new_gdb, new_name)
@@ -50,7 +48,6 @@ provincias = [row[0] for row in arcpy.da.SearchCursor('provincias_lyr', 'NAME_1'
 # Sustituye las comillas simples en los nombres de las provincias por un carácter de escape de comilla simple
 provincias_escaped = [provincia.replace("'", "_") for provincia in provincias]
 
-# Recorre cada provincia
 for provincia in provincias_escaped:
     # Selecciona la provincia actual
     arcpy.SelectLayerByAttribute_management('provincias_lyr', 'NEW_SELECTION', f"NAME_1 = '{provincia}'")
@@ -60,10 +57,18 @@ for provincia in provincias_escaped:
     nombre_raster_salida = nombre_raster_salida.replace(" ", "_").replace("-", "_")
     raster_salida = os.path.join(gdb_salida, nombre_raster_salida)
 
-    # Recorta el raster de entrada según los límites de la provincia actual y guarda el resultado en el raster de salida
-    arcpy.Clip_management(capa_entrada, "#", raster_salida, 'provincias_lyr', "#", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
+    try:
+        # Intenta recortar el raster de entrada según los límites de la provincia actual y guarda el resultado en el raster de salida
+        arcpy.Clip_management(capa_entrada, "#", raster_salida, 'provincias_lyr', "#", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
+    except arcpy.ExecuteError as e:
+        if "ERROR 001566" in str(e):
+            print(f"No se pudo recortar el raster para la provincia {provincia} debido a que está fuera de los límites del raster. Se omite.")
+            continue
+        else:
+            raise
 
 # Elimina la capa temporal de provincias
 arcpy.Delete_management('provincias_lyr')
 
 print("PROCESO TERMINADO")
+
